2d519ba251c66e33654851a189829392477de0df
Ikram Chahdi <120470168+ikc20@users.noreply.github.com>
2024-02-11 23:05:54 +0100

    Integrated CocktailApiService

--- src/app/cocktail-api.service.ts
+++ src/app/cocktail-api.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CocktailApiService {
-  private baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1';
+  private readonly url = 'https://www.thecocktaildb.com/api/json/v1/1';
 
   constructor(private http: HttpClient) { }
 
-  getDrinks(): Observable<any> {
-    const url = `${this.baseUrl}/filter.php?c=Ordinary_Drink`;
-    return this.http.get<any>(url);
+  public getDrinks(): Observable<any> {
+    const endpoint = `${this.url}/filter.php?c=Ordinary_Drink`;
+    return this.http.get<any>(endpoint);
   }
 }
